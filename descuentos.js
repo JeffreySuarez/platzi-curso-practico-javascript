0a081080a67375cd39ecfd7319cc3b328b606e23
JeffreySuarez <79020208+JeffreySuarez@users.noreply.github.com>
2021-10-30 20:22:29 -0500

    inicio del taller 3

--- descuentos.js
+++ descuentos.js
@@ -1,14 +1,34 @@
-const precioOriginal = 100;
-const descuento = 15;
+const precioOriginal = 120;
+const descuento = 18;
 
-const porcentajePrecioConDescuento = 100 - descuento;
+function calcularPrecioConDescuento(precio, descuento) {
+  const porcentajePrecioConDescuento = 100 - descuento;
 
-const precioConDescuento =
-  (precioOriginal * porcentajePrecioConDescuento) / 100;
+  const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
 
-console.log({
-  precioOriginal,
-  precioConDescuento,
-  porcentajePrecioConDescuento,
-  descuento,
-});
+  return precioConDescuento;
+}
+
+calcularPrecioConDescuento();
+
+function onClickButtonPriceDiscount() {
+  const inputPrice = document.querySelector("#inputPrice");
+  const priceValue = inputPrice.value;
+  const inputDiscount = document.querySelector("#inputDiscount");
+  const discountValue = inputDiscount.value;
+
+  const precioConDescuento = calcularPrecioConDescuento(
+    priceValue,
+    discountValue
+  );
+
+  const resultP = document.querySelector("#resultP");
+  resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+}
+
+// console.log({
+//   precioOriginal,
+//   precioConDescuento,
+//   porcentajePrecioConDescuento,
+//   descuento,
+// });
